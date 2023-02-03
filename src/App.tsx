bc19fe7b81d74ccbbde5e2afe1d5ba944d86b805
Alex Navarro <alexnavter@gmail.com>
2023-02-03 18:41:21 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        title="giphy"
+        src="https://giphy.com/embed/lgCvugdD94F6o"
+        width="480"
+        height="382"
+        className="giphy-embed"
+        allowFullScreen
+      />
+      <span>Under construction. Coming soon.</span>
+    </div>
+  );
+}
+
+export default App;
