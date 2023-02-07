b44e5e706aa875a80c82329c4516f13d38434d71
SFontal <sergiot.fontal@gmail.com>
2023-02-07 11:42:41 +0100

    Add actions

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,47 +1,43 @@
 export interface MealStructure {
-  hits: [
-    {
-      recipe: {
-        label: string;
-        image: string;
-        images: {
-          SMALL: {
-            url: string;
-          };
-        };
-        ingredientLines: string[];
-        calories: number;
-        cuisineType: string[];
-        totalNutrients: {
-          FAT: {
-            label: string;
-            quantity: number;
-            unit: string;
-          };
-          CHOCDF: {
-            label: string;
-            quantity: number;
-            unit: string;
-          };
-          SUGAR: {
-            label: string;
-            quantity: number;
-            unit: string;
-          };
-          PROCNT: {
-            label: string;
-            quantity: number;
-            unit: string;
-          };
-          CHOLE: {
-            label: string;
-            quantity: number;
-            unit: string;
-          };
-        };
+  recipe: {
+    label: string;
+    image: string;
+    images: {
+      SMALL: {
+        url: string;
       };
-    }
-  ];
+    };
+    ingredientLines: string[];
+    calories: number;
+    cuisineType: string[];
+    totalNutrients: {
+      FAT: {
+        label: string;
+        quantity: number;
+        unit: string;
+      };
+      CHOCDF: {
+        label: string;
+        quantity: number;
+        unit: string;
+      };
+      SUGAR: {
+        label: string;
+        quantity: number;
+        unit: string;
+      };
+      PROCNT: {
+        label: string;
+        quantity: number;
+        unit: string;
+      };
+      CHOLE: {
+        label: string;
+        quantity: number;
+        unit: string;
+      };
+    };
+  };
 }
 
 export type MealsStructure = MealStructure[];
