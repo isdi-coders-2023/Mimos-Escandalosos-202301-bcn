659be8dc052fa7da7d6e38a7a827378b2ebc5f14
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-07 17:14:37 +0100

    Add Form component structure and resolved conflicts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,9 @@
+import MealForm from "../Form/MealForm";
+
 const App = () => {
   return (
     <div className="App">
-      <iframe
-        title="giphy"
-        src="https://giphy.com/embed/lgCvugdD94F6o"
-        width="480"
-        height="382"
-        className="giphy-embed"
-        allowFullScreen
-      />
-      <span>Under construction. Coming soon.</span>
+      <MealForm />
     </div>
   );
 };
