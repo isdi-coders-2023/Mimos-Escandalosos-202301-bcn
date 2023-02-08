8dfd63ca866d0f741c0871010ed3abb107dd5054
Jairo Vidomines <jvidomines@gmail.com>
2023-02-08 13:29:42 +0100

    Add style to CardList component

---
+++ src/components/CardList/CardListStyled.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const CardListStyled = styled.ul`
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 600px) {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+    flex-direction: row;
+    gap: 20px;
+  }
+
+  .meal-list {
+    padding: 0;
+  }
+`;
+
+export default CardListStyled;
