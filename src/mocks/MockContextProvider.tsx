56c7830c6cd59b27ac6ffc11dc4d3dc1336e9b92
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-09 13:28:11 +0100

    Add custom hook test

---
+++ src/mocks/MockContextProvider.tsx
@@ -0,0 +1,25 @@
+import { MealsStructure } from "../data/types";
+import MealsInfoContext from "../store/contexts/MealsInfoContext/MealsInfoContext";
+
+interface MockContextProviderProps {
+  children: JSX.Element | JSX.Element[];
+  mockStore: RecipeStructure;
+}
+
+interface RecipeStructure {
+  dispatch: React.Dispatch<any>;
+  meals: MealsStructure;
+}
+
+const MockContextProvider = ({
+  children,
+  mockStore,
+}: MockContextProviderProps): JSX.Element => {
+  return (
+    <MealsInfoContext.Provider value={mockStore}>
+      {children}
+    </MealsInfoContext.Provider>
+  );
+};
+
+export default MockContextProvider;
---
+++ src/mocks/mockStore.ts
@@ -0,0 +1,15 @@
+import { MealsStructure } from "../data/types";
+import {
+  MealsAction,
+  LoadMealsAction,
+  MealsActionType,
+} from "../store/actions/types";
+
+export const dispatch: React.Dispatch<MealsAction> = jest.fn();
+export const meals: MealsStructure = [];
+export const mockStore = { dispatch, meals: [] as MealsStructure };
+export const mockDispatch = jest.spyOn(mockStore, "dispatch");
+export const mockLoadMealsAction: LoadMealsAction = {
+  type: MealsActionType.loadMeals,
+  payload: meals,
+};
