7acfe68bfd5cf95b8bea76185b3fdde33fb72e10
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-10 22:19:41 +0100

    Refactor structure of MealForm and add missing labels

--- src/components/MealForm/MealForm.tsx
+++ src/components/MealForm/MealForm.tsx
@@ -4,116 +4,124 @@ const MealForm = (): JSX.Element => {
   return (
     <>
       <MealFormStyled className="form-meal">
-        <input
-          className="form-meal__name"
-          type="text"
-          aria-label="Meal name"
-          placeholder="Meal name"
-          required
-        />
-        <div className="form-meal__nutrients nutrients">
-          <div className="nutrients-card__info">
-            <div className="info-labels">
-              <label htmlFor="Kcal">Kcal</label>
-              <input
-                className="nutrients__kcal"
-                type="text"
-                aria-label="Kcal"
-                placeholder="Kcal"
-                required
-              />
-            </div>
-            <div className="info-labels">
-              <label htmlFor="Cuisine type">Cuisine type</label>
-              <select
-                className="nutrients__type"
-                name="Cuisine type"
-                id="type-select"
-                required
-              >
-                Cuisine type
-              </select>
-            </div>
+        <div className="form-meal__meal-name">
+          <label htmlFor="meal-name">Meal name</label>
+          <input
+            className="form-meal__name"
+            type="text"
+            aria-label="Meal name"
+            placeholder="Meal name"
+            required
+          />
+        </div>
+        <div className="form-meal__nutrients">
+          <div className="info-values">
+            <label htmlFor="Kcal">Kcal</label>
+            <input
+              className="info__data"
+              type="text"
+              aria-label="Kcal"
+              placeholder="Kcal"
+              required
+            />
           </div>
-          <div className="nutrients-info">
-            <div className="info-labels">
-              <label htmlFor="Fat">Fat</label>
+          <div className="info-values">
+            <label htmlFor="Cuisine type">Cuisine type</label>
+            <select className="info__data" id="type-select" required>
+              Cuisine type
+            </select>
+          </div>
+        </div>
+        <div className="form-meal__nutrients-info info">
+          <div className="info-values">
+            <label htmlFor="Fat">Fat</label>
+            <div className="info-unit">
               <input
-                className=" nutrients__fat nutrient"
+                className="info__data"
                 type="text"
                 aria-label="Fat"
                 placeholder="Fat"
                 required
               />
+              <span>mg</span>
             </div>
-            <span>mg</span>
           </div>
-          <div className="nutrients-info">
-            <div className="info-labels">
-              <label htmlFor="Sugar">Sugar</label>
+          <div className="info-values">
+            <label htmlFor="Sugar">Sugar</label>
+            <div className="info-unit">
               <input
-                className=" nutrients__sugar nutrient"
+                className="info__data"
                 type="text"
                 aria-label="Sugar"
                 placeholder="Sugar"
                 required
               />
+              <span>mg</span>
             </div>
-            <span>mg</span>
           </div>
-          <div className="nutrients-info">
-            <div className="info-labels">
-              <label htmlFor="Carbs">Carbs</label>
+        </div>
+        <div className="form-meal__nutrients-info info">
+          <div className="info-values">
+            <label htmlFor="Carbs">Carbs</label>
+            <div className="info-unit">
               <input
-                className="nutrients__carbs nutrient"
+                className="info__data"
                 type="text"
                 aria-label="Carbs"
                 placeholder="Carbs"
                 required
               />
+              <span>mg</span>
             </div>
-            <span>mg</span>
           </div>
-          <div className="nutrients-info">
-            <div className="info-labels">
-              <label htmlFor="Colesterol">Colesterol</label>
+          <div className="info-values">
+            <label htmlFor="Colesterol">Colesterol</label>
+            <div className="info-unit">
               <input
-                className="nutrients__colesterol nutrient"
+                className="info__data"
                 type="text"
                 aria-label="Colesterol"
                 placeholder="Colesterol"
                 required
               />
+              <span>mg</span>
             </div>
-            <span>mg</span>
           </div>
-          <div className="nutrients-info">
-            <div className="info-labels">
-              <label htmlFor="Proteins">Proteins</label>
+        </div>
+        <div className="form-meal__proteins">
+          <div className="info-values">
+            <label htmlFor="Proteins">Proteins</label>
+            <div className="info-unit">
               <input
-                className="nutrients__proteins nutrient"
+                className="info__data"
                 type="text"
                 aria-label="Proteins"
                 placeholder="Proteins"
                 required
               />
+              <span>mg</span>
             </div>
-            <span>mg</span>
           </div>
         </div>
-        <textarea
-          className="form-meal__ingredients"
-          aria-label="Ingredients"
-          placeholder="Ingredients"
-          required
-        />
-        <input
-          className="form-meal__image"
-          type="text"
-          aria-label="Meal image"
-          placeholder="Add your image"
-          required
-        />
+        <div className="form-meal__ingredients ingredients">
+          <label htmlFor="Ingredients">Ingredients</label>
+          <textarea
+            className="ingredients__data"
+            aria-label="Ingredients"
+            placeholder="Ingredients"
+            required
+          />
+        </div>
+        <div className="form-meal__image image">
+          <label htmlFor="Image">Image</label>
+          <input
+            className="image__data"
+            type="text"
+            aria-label="Meal image"
+            placeholder="Add your image"
+            required
+          />
+        </div>
         <div className="form-meal__submit submit">
           <button className="submit__button">Create meal</button>
         </div>
