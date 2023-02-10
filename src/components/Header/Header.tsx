f78f5645fc095226e67da8f0f8d159061d4a9fff
Alex Navarro <alexnavter@gmail.com>
2023-02-10 11:57:16 +0100

    Add styles to Header logo & nav bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,35 +3,47 @@ import HeaderStyled from "./HeaderStyled";
 const Header = (): JSX.Element => {
   return (
     <HeaderStyled className="header">
-      <img
-        src="img/mimo_meals_mobile.png"
-        alt="mimo mobile logo"
-        className="header__mobileImage"
-        height={47}
-        width={36}
-      />
-      <img
-        src="img/mimo_meals.png"
-        alt="mimo desktop logo"
-        className="header__desktopImage"
-        height={106}
-        width={269}
-      />
+      <a href="/home" className="header__mobile__link">
+        <img
+          src="img/mimo_meals_mobile.png"
+          alt="mimo mobile logo"
+          className="header__mobile__image"
+          height={47}
+          width={36}
+        />
+      </a>
+      <a href="/home" className="header__desktop__link">
+        <img
+          src="img/mimo_meals.png"
+          alt="mimo desktop logo"
+          className="header__desktop__image"
+          height={80}
+          width={203}
+        />
+      </a>
       <nav className="header__nav-bar nav-bar">
         <div className="nav-bar__element">
-          <i className="fa-solid fa-house-chimney fa-solid--active"></i>
+          <a href="/home" className="nav-bar__icon">
+            <i className="fa-solid fa-house-chimney fa-solid--active"></i>
+          </a>
           <a className="nav-bar__text" href="/home">
             Home
           </a>
         </div>
+
         <div className="nav-bar__element">
-          <i className="fa-solid fa-heart fa-solid--active"></i>
+          <a href="/home" className="nav-bar__icon">
+            <i className="fa-solid fa-heart fa-solid--active"></i>
+          </a>
           <a className="nav-bar__text" href="/favourites">
             Favourites
           </a>
         </div>
+
         <div className="nav-bar__element">
-          <i className="fa-solid fa-plus fa-solid--active"></i>
+          <a href="/home" className="nav-bar__icon">
+            <i className="fa-solid fa-plus fa-solid--active"></i>
+          </a>
           <a className="nav-bar__text" href="/create-meal">
             Create meal
           </a>
