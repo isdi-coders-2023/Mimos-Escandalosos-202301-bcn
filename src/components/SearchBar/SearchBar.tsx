36ddcfbcf0c7c2ed8c621a60d848bfd7ba20dece
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-10 16:02:00 +0100

    Create SearchBar component

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,10 @@
+export const SearchBar = (): JSX.Element => {
+  return (
+    <input
+      type="text"
+      aria-label="search"
+      placeholder="Search meal"
+      className="search-bar"
+    />
+  );
+};
