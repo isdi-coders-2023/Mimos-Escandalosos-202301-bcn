e714f8d6de90cd6cf1a0c25247e36c3086051c34
Jairo Vidomines <jvidomines@gmail.com>
2023-02-10 17:08:30 +0100

    Add style to MealAdded component

---
+++ src/components/MealAdded/MealAddedStyled.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const MealAddedStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 14px;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  gap: 10px;
+
+  .meal-added-container {
+    display: flex;
+    background-color: #94cf97;
+    margin: auto;
+  }
+
+  i {
+    font-size: 18px;
+  }
+
+  .meal-added-content__icon {
+    display: flex;
+    color: #94cf97;
+    font-size: 20px;
+  }
+
+  .meal-added-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid #1cb802;
+    border-radius: 10px;
+    width: 250px;
+    height: 90px;
+    gap: 10px;
+  }
+`;
+
+export default MealAddedStyled;
