1711d6990784a0fcacb728703e69ac43f58dbd92
Alex Navarro <alexnavter@gmail.com>
2023-02-10 17:24:47 +0100

    Error 404 test

---
+++ src/components/Error404/Error404.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Error404 from "./Error404";
+
+describe("Given a Error 404 component", () => {
+  describe("When the request does not found the return a response", () => {
+    test("Then it should show an image of the 404 error", () => {});
+    render(<Error404 />);
+
+    const errorImg = screen.getByRole("img");
+
+    expect(errorImg).toBeInTheDocument();
+  });
+});
