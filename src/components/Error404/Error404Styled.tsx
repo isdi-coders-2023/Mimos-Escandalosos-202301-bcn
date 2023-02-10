8299f80165b093fcfc3e582ff4532e82ac6159a8
Alex Navarro <alexnavter@gmail.com>
2023-02-10 16:32:37 +0100

    Create Error 404

---
+++ src/components/Error404/Error404Styled.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const ErrorNotFoundStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  width: 100%;
+  height: 100vh;
+  margin-top: 20px;
+
+  .error {
+    padding: 20px;
+
+    &__image__desktop {
+      display: none;
+    }
+
+    &__button {
+      width: 350px;
+      height: 35px;
+      border: none;
+      border-radius: 10px;
+      background-color: #f7af30;
+      color: black;
+    }
+  }
+
+  @media (min-width: 750px) {
+    .error {
+      &__image__mobile {
+        display: none;
+      }
+
+      &__image__desktop {
+        display: flex;
+      }
+
+      &__button {
+        cursor: pointer;
+      }
+    }
+  }
+`;
+
+export default ErrorNotFoundStyled;
