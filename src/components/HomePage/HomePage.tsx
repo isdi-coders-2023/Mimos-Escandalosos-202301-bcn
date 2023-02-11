d2afeb0c71277cceff3c7f5335452c835f900b50
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-11 12:41:23 +0100

    Create HomePage component

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,24 @@
+import { useContext, useEffect } from "react";
+import { useApi } from "../../hook/useApi";
+import MealsInfoContext from "../../store/contexts/MealsInfoContext/MealsInfoContext";
+import CardList from "../CardList/CardList";
+import { SearchBar } from "../SearchBar/SearchBar";
+
+export const HomePage = (): JSX.Element => {
+  const { getApiData } = useApi();
+
+  const { meals } = useContext(MealsInfoContext);
+
+  useEffect(() => {
+    getApiData();
+  }, [getApiData]);
+
+  return (
+    <>
+      <h1>What do you want to cook?</h1>
+      <span>Enter some ingredients</span>
+      <SearchBar />
+      <CardList meals={meals} />
+    </>
+  );
+};
