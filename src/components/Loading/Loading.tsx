8cce4e7d9d835bf5ce57f187dae65ce63631196f
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-11 17:50:25 +0100

    Add loader to production and refactor Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,9 +1,14 @@
 import LoadingStyled from "./LoadingStyled";
+import { SuperBalls } from "@uiball/loaders";
 
 const Loading = (): JSX.Element => {
   return (
-    <LoadingStyled aria-label="The page is loading" role="dialog">
-      <span className="loader"></span>
+    <LoadingStyled
+      className="loading-page"
+      aria-label="The page is loading"
+      role="dialog"
+    >
+      <SuperBalls />
     </LoadingStyled>
   );
 };
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,11 +1,17 @@
 import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
+import Loading from "../components/Loading/Loading";
+import { UiContext } from "../store/contexts/uiContext/UiContext";
+import { useContext } from "react";
 
 const Layout = () => {
+  const { isLoading } = useContext(UiContext);
+
   return (
     <>
       <Header />
       <main>
+        {isLoading && <Loading />}
         <Outlet />
       </main>
     </>
