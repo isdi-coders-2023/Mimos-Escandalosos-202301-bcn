82627e1d50e8878e9a08148169a233cd2ad6aa23
SFontal <sergiot.fontal@gmail.com>
2023-02-12 08:22:18 +0100

    Fix anchor nested in other anchor

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,28 +4,24 @@ import HeaderStyled from "./HeaderStyled";
 const Header = (): JSX.Element => {
   return (
     <HeaderStyled className="header">
-      <a href="/home" className="header__mobile__link">
-        <Link to="/">
-          <img
-            src="img/mimo_meals_mobile.png"
-            alt="mimo mobile logo"
-            className="header__mobile__image"
-            height={47}
-            width={36}
-          />
-        </Link>
-      </a>
-      <a href="/home" className="header__desktop__link">
-        <Link to="/">
-          <img
-            src="img/mimo_meals.png"
-            alt="mimo desktop logo"
-            className="header__desktop__image"
-            height={80}
-            width={203}
-          />
-        </Link>
-      </a>
+      <Link to="/">
+        <img
+          src="img/mimo_meals_mobile.png"
+          alt="mimo mobile logo"
+          className="header__mobile__image"
+          height={47}
+          width={36}
+        />
+      </Link>
+      <Link to="/">
+        <img
+          src="img/mimo_meals.png"
+          alt="mimo desktop logo"
+          className="header__desktop__image"
+          height={80}
+          width={203}
+        />
+      </Link>
       <nav className="header__nav-bar nav-bar">
         <div className="nav-bar__element">
           <Link to="/" className="nav-bar__icon">
