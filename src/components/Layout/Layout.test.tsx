faa3570d459a2c0fc4f240c34aa0c495743ba270
victorandujar <victorandujarmurcia@gmail.com>
2023-02-12 17:51:57 +0100

    Refactor layout test

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import { UiContextProvider } from "../../store/contexts/uiContext/UiContextProvider";
 import Layout from "./Layout";
 
 describe("Given a Layout", () => {
   describe("When it renders", () => {
     test("Then it should show the web logo", () => {
       render(
-        <BrowserRouter>
-          <Layout />
-        </BrowserRouter>
+        <UiContextProvider>
+          <BrowserRouter>
+            <Layout />
+          </BrowserRouter>
+        </UiContextProvider>
       );
 
       const expectedLogo = screen.getByRole("img");
